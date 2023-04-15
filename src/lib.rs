c0b7656a78549288630b694a0a2cb4d71c6bf7bc
Thomas Niederberger <thomas@niederb.ch>
2023-04-15 23:01:10 +0200

    Add simple StopWatch

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 pub mod interval_checker;
 pub mod measurement;
+pub mod stop_watch;
 
 pub use interval_checker::*;
 pub use measurement::*;
+pub use stop_watch::StopWatch;
---
+++ src/stop_watch.rs
@@ -0,0 +1,37 @@
+use std::time::{Duration, Instant};
+
+pub struct StopWatch {
+    start_time: Option<Instant>,
+    elapsed: Duration,
+}
+
+impl StopWatch {
+    pub fn new() -> Self {
+        return Self { start_time: None, elapsed: Duration::default() }
+    }
+
+    pub fn start(&mut self) {
+        self.start_time = Some(Instant::now());
+    }
+
+    pub fn stop(&mut self) {
+        self.elapsed = self.elapsed();
+        self.start_time = None;
+    }
+
+    pub fn reset(&mut self) {
+        self.start_time = None;
+        self.elapsed = Duration::default();
+    }
+
+    pub fn elapsed(&self) -> Duration {
+        match self.start_time {
+            Some(t) => t.elapsed() + self.elapsed,
+            None => self.elapsed,
+        }
+    }
+
+    pub fn is_running(&self) -> bool {
+        self.start_time.is_some()
+    }
+}
